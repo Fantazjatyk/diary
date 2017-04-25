0e7c454f029d3561b993923fb21856202a55668a
Fantazjatyk <michal.szymanski.aajar@gmail.com>
2017-04-25 21:35:57 +0200

    added licence headers

--- src/main/java/pl/diary/dao/note/NoteSearcher.java
+++ src/main/java/pl/diary/dao/note/NoteSearcher.java
@@ -1,7 +1,7 @@
-/*
+/* 
  * The MIT License
  *
- * Copyright 2017 Micha� Szyma�ski, kontakt: michal.szymanski.aajar@gmail.com.
+ * Copyright 2017 Michał Szymański, kontakt: michal.szymanski.aajar@gmail.com.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
